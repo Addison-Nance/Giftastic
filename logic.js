53c1a4f0ba8148ecfc65a2c822f9f29bca1e7a55
Addison Nance <madi.nance911@gmail.com>
2020-04-05 16:02:14 -0600

    added API key and scripted javascript

--- logic.js
+++ logic.js
@@ -1,9 +1,16 @@
 var searchterm="";
-$("#searchbutton").on("click", function{
+$("#searchbutton").on("click", function(){
     searchterm= $("#searchinput").val();
     var queryURL= "api.giphy.com/v1/gifs/search?q=" + searchterm + "&api_key=wQJXIAQWtZyJ9vxRJKD1Kly5kx8U1jQF&limit=5";
-    
+    console.log(searchterm);
+    $.ajax({
+        url: queryURL,
+        mathod: "GET"
+    })
+    .then(function(response){
+        var results =response.data;
 
+    })
 
 
 });
