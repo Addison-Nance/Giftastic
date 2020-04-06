c6c03443949f974c933e80d5b1b8f2443af88269
Addison Nance <madi.nance911@gmail.com>
2020-04-05 19:23:16 -0600

    psudocoding remaining tasks to complete

--- logic.js
+++ logic.js
@@ -28,9 +28,28 @@ $(document).on('click', '.moviebutton', function(){
 
     var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=" + searchterm + "&api_key=wQJXIAQWtZyJ9vxRJKD1Kly5kx8U1jQF&limit=5");
     xhr.done(function(data) { console.log("success got data", data)});
-    
+        for (let i = 0; i < array.length; i++) {
+            //grap the url from the response
+            //write the URL to the DOM within an img src tag
+            //dispaly the gif in a "still" state
+            // display the gif's rating below the gif
+
+        }
     
 
 
 
 });
+// on click function
+// if state is "still":
+//set state to "animated" and animate gif
+// else: 
+// set state to still
+//display still image
+
+
+
+//capture search input
+//push to topics array
+//call function to recreate search buttons with new strings
+//
